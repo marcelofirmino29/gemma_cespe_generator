5f08aaf35e0780c0058713c5833f0d6a43ad3906
marcelofirmino29 <marc.firmino@gmail.com>
2025-04-25 21:41:22 -0400

    Empacotamento: Mais configurações devido a erros de importação

--- manage.py
+++ manage.py
@@ -1,35 +1,13 @@
 #!/usr/bin/env python
-# """Django's command-line utility for administrative tasks."""
-# import os
-# import sys
 
-
-# def main():
-#     """Run administrative tasks."""
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-#     try:
-#         from django.core.management import execute_from_command_line
-#     except ImportError as exc:
-#         raise ImportError(
-#             "Couldn't import Django. Are you sure it's installed and "
-#             "available on your PYTHONPATH environment variable? Did you "
-#             "forget to activate a virtual environment?"
-#         ) from exc
-#     execute_from_command_line(sys.argv)
-
-
-# if __name__ == '__main__':
-#     main()
-
-
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 def main():
     """Run administrative tasks."""
     # --- CORREÇÃO AQUI ---
-    # Certifique-se de que 'gemma_cespe_generator' é o nome correto da pasta
-    # que contém o seu arquivo settings.py
+    # Alterado de 'docling_django.settings' para o nome correto do seu projeto
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gemma_cespe_generator.settings')
     # --- FIM DA CORREÇÃO ---
     try:
